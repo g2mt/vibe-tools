4109b2d6930408623ea9a5f5b22432ad5ebdb86d
T. M. <pm2mtr@gmail.com>
2026-04-12 03:21:04 +0000

    add ipa quiz data

---
+++ docs/ipa-quiz/data/README.md
@@ -0,0 +1,3 @@
+Download the en_US.json dictionary from here:
+https://github.com/open-dict-data/ipa-dict/releases/tag/1.0
+
---
+++ docs/ipa-quiz/data/preprocess.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("./en_US.json", "utf8"));
+
+const vocab = data["en_US"][0];
+const chars = {};
+const exceptions = "/ˈ,ˌ";
+const newVocab = {};
+for (const word in vocab) {
+  const ipa = vocab[word];
+  let newIpa = "";
+  for (const ch of ipa) {
+    if (exceptions.indexOf(ch) > -1)
+      continue;
+    if(typeof chars[ch] === "undefined")
+      chars[ch] = 0;
+    chars[ch] += 1;
+    newIpa += ch;
+  }
+  newVocab[word]=newIpa;
+}
+fs.writeFileSync("./freq.json", JSON.stringify(chars), "utf8");
+fs.writeFileSync("./chars.json", JSON.stringify(Object.keys(chars)), "utf8");
+fs.writeFileSync("./en_US_processed.json", JSON.stringify(newVocab), "utf8");
